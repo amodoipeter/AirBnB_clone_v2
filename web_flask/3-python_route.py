d0848a72832f20de201e1bf7a1f2a90b5f5bb792
Amodoi Peter <amodoipeter@gmail.com>
2022-10-15 23:46:19 +0300

    route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def c(text):
 
 
 @app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
     """Displays 'Python' followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
