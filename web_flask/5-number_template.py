bca1dec1bedcbc6aaa514daeb0d04ccad6407090
Amodoi Peter <amodoipeter@gmail.com>
2022-10-19 04:02:53 +0300

    template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,12 +38,11 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def var_num_template(n):
-        """
-            function to display number in html page
-        """
-        return render_template("5-number.html", n=n)
-
+def number_template(n):
+    """Returns a template at the /number_template/<n> route,
+    expanding route"""
+    if type(n) == int:
+        return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
